8e7c88c6530ba7f71dcc1e87a38eb208bad6c1f0
Fred Morrison <fpmorrison@verizon.net>
2017-04-25 17:22:04 -0400

    aplit root-level route definitions into their own const

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule }              from '@angular/core';
 import { RouterModule }          from '@angular/router';
 
-// these are references in the forRoot call below:
+// these are referenced a components in the AppRoutes definition, so we must import them:
 import { WelcomeComponent }      from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
+const AppRoutes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
   imports:[
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent }
-    ])
+    RouterModule.forRoot(AppRoutes)
   ],
   exports: [ RouterModule ]
 })
