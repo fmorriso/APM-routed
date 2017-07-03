63c9cff7b0e7a101866189f8e4d931f1d73a1c80
Fred Morrison <fpmorrison@verizon.net>
2017-07-03 14:50:14 -0400

    Finished adding Route Guards (module 11)

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,33 +1,38 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 @Component({
-    templateUrl: './login.component.html'
+	templateUrl: './login.component.html'
 })
 export class LoginComponent {
-    errorMessage: string;
-    pageTitle = 'Log In';
-    userName: string;
-    password: string;
+	errorMessage: string;
+	pageTitle = 'Log In';
+	userName: string;
+	password: string;
 
-    constructor(private authService: AuthService,
-                private router: Router) { }
+	constructor(private authService: AuthService,
+	            private router: Router) {
+	}
 
-    login(loginForm: NgForm) {
-        if (loginForm && loginForm.valid) {
-            //console.log('login userName=' + loginForm.form.value.userName);
-            this.userName = loginForm.form.value.userName;
-            //console.log('login password=' + loginForm.form.value.password);
-            this.password = loginForm.form.value.password;
-            this.authService.login(this.userName, this.password);
+	login(loginForm: NgForm) {
+		if (loginForm && loginForm.valid) {
+			//console.log('login userName=' + loginForm.form.value.userName);
+			this.userName = loginForm.form.value.userName;
+			//console.log('login password=' + loginForm.form.value.password);
+			this.password = loginForm.form.value.password;
+			this.authService.login(this.userName, this.password);
 
-            // Navigate to the Product List page after successful log in.
-            this.router.navigate(['/products']);
-        } else {
-            this.errorMessage = 'Please enter a user name and password.';
-        }
-    }
+			// Navigate to the Product List page after successful log in.
+			if (this.authService.redirectUrl) {
+				this.router.navigateByUrl(this.authService.redirectUrl);
+			} else {
+				this.router.navigate(['/products']);
+			}
+		} else {
+			this.errorMessage = 'Please enter a user name and password.';
+		}
+	}
 }
