57e71add78014652a110f7db463206ec060d3875
Fred Morrison <fpmorrison@verizon.net>
2017-07-05 11:26:44 -0400

    Added SelectiveStrategy service to customize background preloading of feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,42 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-// These are referenced as components in the AppRoutes definition, so we must import them:
-import {WelcomeComponent} from './home/welcome.component';
-import {PageNotFoundComponent} from './page-not-found.component';
-import {AuthGuard} from './user/auth-guard.service';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// our custom feature module preloading service
+import { SelectiveStrategy } from './selective-strategy.service';
+
+// These are referenced as components in the AppRoutes definition, so we must import them:
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth-guard.service';
+
+//NOTE: the canLoad allows us to avoid downloading the entire Products components suite
+//      until/unless the user successfully logs in.
+//      We further control the loading process by using our custom preloading
+//      strategy service to asynchronously background load the
+//      Products component suite after the successful login.
 const topLevelRoutes: Routes = [
 	{path: 'welcome', component: WelcomeComponent},
 	{
 		path: 'products',
-		//canActivate: [AuthGuard],
-		//data: {preload: true},
+		canLoad: [AuthGuard],
+		data: { preload: true },
 		loadChildren: 'app/products/product.module#ProductModule'
 	},
 	{path: '', redirectTo: 'welcome', pathMatch: 'full'},
 	{path: '**', component: PageNotFoundComponent}
 ];
 
-const rootConfig = {enableTracing: true};
+const rootConfig = {
+	enableTracing: true,
+	preloadingStragegy: SelectiveStrategy
+};
 
 @NgModule({
 	imports: [
 		RouterModule.forRoot(topLevelRoutes, rootConfig)
 	],
-	exports: [RouterModule]
+	exports: [RouterModule],
+	providers: [SelectiveStrategy]
 })
 export class AppRoutingModule {
 }
---
+++ src/app/selective-strategy.service.ts
@@ -0,0 +1,34 @@
+// This service is used throughout the entire application
+import {Injectable} from '@angular/core';
+import {PreloadingStrategy, Route} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class SelectiveStrategy implements PreloadingStrategy {
+
+	constructor() {	}
+
+	preload(route: Route, load: Function): Observable<any> {
+		// check our custom data element, named preload to see if we should preload or not.
+		// That value is set inside the JSON definition of the app-routing.module.ts.
+		// Example:
+		/*
+			{
+				path: 'products',
+				canLoad: [AuthGuard],
+				data: {preload: true},
+				loadChildren: 'app/products/product.module#ProductModule'
+			}
+		 */
+		// The above is coupled with our custom preloading strategy that is defined
+		// as the second piece of JSON that is passed to RouterModule.forRoute()
+		// inside app-routing.module.ts
+		console.log(`SelectiveStrategy - route: ${JSON.stringify(route)}`);
+		if (route.data && route.data['preload']) {
+			return load();
+		}
+		return Observable.of(null);
+	}
+
+}
